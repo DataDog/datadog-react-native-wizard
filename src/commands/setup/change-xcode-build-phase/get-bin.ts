50f6a5e4e5cd15e50c6e66f97fd2e072b0f13d1e
louiszawadzki <louis.zawadzki@datadoghq.com>
2022-06-29 15:46:09 +0200

    Get correct path from output

--- src/commands/setup/change-xcode-build-phase/get-bin.ts
+++ src/commands/setup/change-xcode-build-phase/get-bin.ts
@@ -1,13 +1,22 @@
 import { exec } from "child_process";
+import { EOL } from "os";
 
 export const getBin = (command: string): Promise<string> => {
   return new Promise((resolve, reject) => {
-    exec(`which ${command}`, (error, stdout, stderr) => {
+    exec(`which -a ${command}`, (error, stdout, stderr) => {
       if (error) {
         reject(error);
       }
       if (stdout) {
-        resolve(stdout);
+        // Because yarn adds a temp path in the user path, we have to remove it
+        // e.g. /var/folders/34/_7q54_lx4nl1cvkjwr3_k4lw0000gq/T/yarn--1656510083555-0.9818920348655578/yarn
+        const paths = stdout.split(EOL);
+        const goodPath = paths.find((path) => !path.match("yarn--"));
+        if (goodPath === undefined) {
+          reject(`Command ${command} is not in path`);
+          return;
+        }
+        resolve(goodPath);
       }
       if (stderr) {
         reject(stderr);
