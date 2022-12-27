8d972989621b938d1650cbf2631f86970ef5c409
louiszawadzki <louis.zawadzki@datadoghq.com>
2022-12-27 16:13:39 +0100

    Support sourcemaps script injection for RN 0.71

--- src/commands/setup/apply-gradle-task/__tests__/inject-task-in-app-build-gradle-file.test.ts
+++ src/commands/setup/apply-gradle-task/__tests__/inject-task-in-app-build-gradle-file.test.ts
@@ -1,4 +1,5 @@
 import { unlinkSync } from "fs";
+import { getRNVersion } from "../../utils/dependency-utils";
 import { getAbsolutePath } from "../../__test-utils__/get-absolute-path";
 import { injectTaskInAppBuildGradleFile } from "../inject-task-in-app-build-gradle-file";
 
@@ -14,15 +15,30 @@ afterEach(() => {
 });
 
 describe("injectTaskInAppBuildGradleFile", () => {
-  it("edits the app/build.gradle file", async () => {
+  it("edits the app/build.gradle file when React Native version is 0.69", async () => {
     await injectTaskInAppBuildGradleFile(
-      getAbsolutePath(`${fixturesPath}/build.gradle`),
-      outputBuildGradle
+      getAbsolutePath(`${fixturesPath}/build.rn69.gradle`),
+      outputBuildGradle,
+      { reactNativeVersion: { major: 0, minor: 69, patch: "0" } }
     );
     // @ts-ignore
     expect(outputBuildGradle).toMatchFile(
       getAbsolutePath(
-        "src/commands/setup/apply-gradle-task/__tests__/results/build.gradle"
+        "src/commands/setup/apply-gradle-task/__tests__/results/build.rn69.gradle"
+      )
+    );
+  });
+
+  it("edits the app/build.gradle file when React Native version is 0.71", async () => {
+    await injectTaskInAppBuildGradleFile(
+      getAbsolutePath(`${fixturesPath}/build.rn71.gradle`),
+      outputBuildGradle,
+      { reactNativeVersion: { major: 0, minor: 71, patch: "0" } }
+    );
+    // @ts-ignore
+    expect(outputBuildGradle).toMatchFile(
+      getAbsolutePath(
+        "src/commands/setup/apply-gradle-task/__tests__/results/build.rn71.gradle"
       )
     );
   });
