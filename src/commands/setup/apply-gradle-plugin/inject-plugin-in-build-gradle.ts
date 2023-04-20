a80c63edf6c12c86df1710f508513650e0a63103
louiszawadzki <louis.zawadzki@datadoghq.com>
2023-04-20 14:55:37 +0200

    Add application variant loop if missing in build.gradle

--- src/commands/setup/apply-gradle-plugin/inject-plugin-in-build-gradle.ts
+++ src/commands/setup/apply-gradle-plugin/inject-plugin-in-build-gradle.ts
@@ -2,12 +2,40 @@ import { EOL } from "os";
 import { GradlePluginNotAutomated, GradlePluginNotInstalled } from "../errors";
 import { editFile } from "../utils/edit-file";
 
+/**
+ * This enum represents the state of the automation injection process.
+ * First we start in the BEFORE_ANDROID_CLOSURE state.
+ * When we enter the `android` closure, we go in IN_ANDROID_CLOSURE state.
+ *
+ * If then we find the `applicationVariants` loop, we inject the automation and go into DONE state.
+ * If we find the end of the android closure and we're still in the IN_ANDROID_CLOSURE state (meaning
+ * there were no `applicationVariants` loop), we inject the automation and go into DONE state.
+ */
+const enum AutomationState {
+  BEFORE_ANDROID_CLOSURE = "BEFORE_ANDROID_CLOSURE",
+  IN_ANDROID_CLOSURE = "IN_ANDROID_CLOSURE",
+  DONE = "DONE",
+}
+
+const AUTOMATION_BLOCK = [
+  `        if (project.tasks.findByName("minify\${variant.name.capitalize()}WithR8")) {`,
+  `            tasks["minify\${variant.name.capitalize()}WithR8"].finalizedBy { tasks["uploadMapping\${variant.name.capitalize()}"] }`,
+  `        }`,
+];
+
+const AUTOMATION_BLOCK_WITH_LOOP = [
+  `    applicationVariants.all { variant ->`,
+  ...AUTOMATION_BLOCK,
+  `    }`,
+];
+
 export const injectPluginInBuildGradle = async (
   androidAppBuildGradleInputFile: string,
   androidAppBuildGradleOutputFile: string
 ) => {
   let hasAddedPluginAndConfiguration = false;
-  let hasAddedAutomation = false;
+  let hasAddedAutomationState: AutomationState =
+    AutomationState.BEFORE_ANDROID_CLOSURE;
   await editFile(
     androidAppBuildGradleInputFile,
     androidAppBuildGradleOutputFile,
@@ -28,16 +56,30 @@ export const injectPluginInBuildGradle = async (
         return `${installationBlock}${EOL}${line}`;
       }
 
-      if (line.match("applicationVariants.all") && !hasAddedAutomation) {
-        hasAddedAutomation = true;
-        const automationBlock = [
-          `        if (project.tasks.findByName("minify\${variant.name.capitalize()}WithR8")) {`,
-          `            tasks["minify\${variant.name.capitalize()}WithR8"].finalizedBy { tasks["uploadMapping\${variant.name.capitalize()}"] }`,
-          `        }`,
-          ``,
-        ].join(EOL);
+      // Entering the android closure
+      if (
+        line.match(/^android {/) &&
+        hasAddedAutomationState === AutomationState.BEFORE_ANDROID_CLOSURE
+      ) {
+        hasAddedAutomationState = AutomationState.IN_ANDROID_CLOSURE;
+      }
 
-        return `${line}${EOL}${automationBlock}`;
+      // Entering the applicationVariants loop
+      if (
+        line.match("applicationVariants.all") &&
+        hasAddedAutomationState === AutomationState.IN_ANDROID_CLOSURE
+      ) {
+        hasAddedAutomationState = AutomationState.DONE;
+        return `${line}${EOL}${AUTOMATION_BLOCK.join(EOL)}${EOL}`;
+      }
+
+      // Reaching the end of the android closure and no applicationVariants loop was found
+      if (
+        line.match(/^}$/) &&
+        hasAddedAutomationState === AutomationState.IN_ANDROID_CLOSURE
+      ) {
+        hasAddedAutomationState = AutomationState.DONE;
+        return `${EOL}${AUTOMATION_BLOCK_WITH_LOOP.join(EOL)}${EOL}${line}`;
       }
 
       return line;
@@ -47,7 +89,7 @@ export const injectPluginInBuildGradle = async (
   if (!hasAddedPluginAndConfiguration) {
     throw new GradlePluginNotInstalled();
   }
-  if (!hasAddedAutomation) {
+  if ((hasAddedAutomationState as AutomationState) !== AutomationState.DONE) {
     throw new GradlePluginNotAutomated();
   }
 };
