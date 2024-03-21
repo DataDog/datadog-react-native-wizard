32ea318234384ede789f76766f16d0ec17f841f5
louiszawadzki <louis.zawadzki@datadoghq.com>
2024-03-21 14:58:33 +0100

    Support sourcemaps upload for RN 0.74

--- src/commands/setup/change-xcode-build-phase/xcode-build-phase-editor/rn69/rn69-build-phase-editor.ts
+++ src/commands/setup/change-xcode-build-phase/xcode-build-phase-editor/rn69/rn69-build-phase-editor.ts
@@ -3,6 +3,16 @@ import { editFile } from "../../../utils/edit-file";
 import { XCodeBuildPhaseEditor } from "../xcode-build-phase-editor";
 
 /**
+ * This is the script we need to replace from RN 0.69 to RN 0.73 (included).
+ */
+const RN69_SCRIPT = `REACT_NATIVE_XCODE=\\"../node_modules/react-native/scripts/react-native-xcode.sh\\"`;
+
+/**
+ * This is the script we need to replace starting from RN 0.74.
+ */
+const RN74_SCRIPT = `REACT_NATIVE_XCODE=\\"$REACT_NATIVE_PATH/scripts/react-native-xcode.sh\\"`;
+
+/**
  * This script is supporting versions of React Native from 0.69.
  *
  * The goal is to keep all the things inside the script (like custom env variables), and replace
@@ -11,6 +21,22 @@ import { XCodeBuildPhaseEditor } from "../xcode-build-phase-editor";
  * We also need to create a script file, so we ask the user about the ideal location.
  */
 export class RN69BuildPhaseEditor extends XCodeBuildPhaseEditor {
+  private minorRNVersion: number;
+
+  constructor(
+    options: {
+      absoluteProjectPath: string;
+      inputPbxprojFile: string;
+      outputPbxprojFile: string;
+    },
+    otherOptions: {
+      minorRNVersion: number;
+    }
+  ) {
+    super(options);
+    this.minorRNVersion = otherOptions.minorRNVersion;
+  }
+
   private datadogScriptLocation: string = `${this.absoluteProjectPath}/ios/datadog-sourcemaps.sh`;
 
   public editBuildPhase = async () => {
@@ -32,7 +58,7 @@ export class RN69BuildPhaseEditor extends XCodeBuildPhaseEditor {
 
   protected getNewShellScript = (line: string) => {
     const [beforeScript, afterScript] = line.split(
-      `REACT_NATIVE_XCODE=\\"../node_modules/react-native/scripts/react-native-xcode.sh\\"`
+      this.minorRNVersion < 74 ? RN69_SCRIPT : RN74_SCRIPT
     );
     const datadogScript = `REACT_NATIVE_XCODE=\\"./datadog-sourcemaps.sh\\"\\nexport SOURCEMAP_FILE=$DERIVED_FILE_DIR/main.jsbundle.map\\n`;
 
